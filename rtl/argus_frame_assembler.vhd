8ea1f87c7868fa40fceec1f4c4ef89aabedbb031
Max-Gabriel Susman <gabe.susman@gmail.com>
2026-09-22 04:21:10 -0600

    add CH_PER_CHIP generic to argus_frame_assembler

--- rtl/argus_frame_assembler.vhd
+++ rtl/argus_frame_assembler.vhd
@@ -3,8 +3,8 @@
 
 entity argus_frame_assembler is
     generic(
-        CHIP_COUNT : natural := 3;
--- TODO: needs impl
+        CHIP_COUNT  : natural := 3;
+        CH_PER_CHIP : natural := 32
     );
 -- TODO: needs impl
 end entity argus_frame_assembler;
